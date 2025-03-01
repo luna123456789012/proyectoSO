76eb8557d93e7cd42837365c44921f6e9c3f1365
Luna Sousa <luna.sousa@estudiantat.upc.edu>
2025-03-01 12:47:47 +0100

    Entrega 3.3

--- proyecto_so/dev33.sql
+++ proyecto_so/dev33.sql
@@ -3,11 +3,12 @@ CREATE DATABASE juego;
 USE juego;
 CREATE TABLE jugadores (
 	ID_j INT PRIMARY KEY NOT NULL,
-	usuario TEXT NOT NULL
+	usuario TEXT NOT NULL,
+	contraseña TEXT,
 )ENGINE = InnoDB;
 
-INSERT INTO jugadores (ID_j, usuario) VALUES (1, 'alvaro');
-INSERT INTO jugadores (ID_j, usuario) VALUES (2, 'marta');
-INSERT INTO jugadores (ID_j, usuario) VALUES (3, 'paula');
-INSERT INTO jugadores (ID_j, usuario) VALUES (4, 'didac');
-INSERT INTO jugadores (ID_j, usuario) VALUES (5, 'daniel');
+INSERT INTO jugadores (ID_j, usuario, contraseña) VALUES (1, 'alvaro', 'piano42');
+INSERT INTO jugadores (ID_j, usuario, contraseña) VALUES (2, 'marta', '5pistacho');
+INSERT INTO jugadores (ID_j, usuario, contraseña) VALUES (3, 'paula', '86flor');
+INSERT INTO jugadores (ID_j, usuario, contraseña) VALUES (4, 'didac', 'casa00');
+INSERT INTO jugadores (ID_j, usuario, contraseña) VALUES (5, 'daniel', 'so2025');
